2d40a857799d7eb9d14b394a388bc0912c6536a0
MikoZ321 <0123456789miko@gmail.com>
2025-03-16 02:06:33 +0100

    add data stream

--- groundStation/data/script.js
+++ groundStation/data/script.js
@@ -1,5 +1,21 @@
 var ws = new WebSocket("ws://" + window.location.host + "/ws");
 
 ws.onmessage = function(event) {
-    document.getElementById("loradata").innerText = event.data;
+    const data = JSON.parse(event.data);
+    document.getElementById("temperature").innerText = data.temperature;
+    document.getElementById("pressure").innerText = data.pressure;
+    document.getElementById("humidity").innerText = data.humidity;
+    document.getElementById("altitude").innerText = data.altitudeGPS;
+    document.getElementById("latitude").innerText = data.latitude;
+    document.getElementById("longitude").innerText = data.longitude;
+    document.getElementById("lightLevel").innerText = data.lightLevel;
+    document.getElementById("batteryVoltage").innerText = data.batteryVoltage;
+    document.getElementById("motorOutputVoltage").innerText = data.motorOutputVoltage;
+    document.getElementById("gyroX").innerText = data.gyro.x;
+    document.getElementById("gyroY").innerText = data.gyro.y;
+    document.getElementById("gyroZ").innerText = data.gyro.z;
+    document.getElementById("accelX").innerText = data.acceleration.x;
+    document.getElementById("accelY").innerText = data.acceleration.y;
+    document.getElementById("accelZ").innerText = data.acceleration.z;
+    document.getElementById("angularSpeed").innerText = data.angularSpeed;
 };
--- groundStation/groundStation.ino
+++ groundStation/groundStation.ino
@@ -1,9 +1,34 @@
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
 #include <LoRa.h>
 #include <SPI.h>
 #include <WiFi.h>
+
+struct vector3D {
+  float x;
+  float y;
+  float z;
+};
+
+struct dataContainer {
+  long tickCount;
+  float temperature; // [temperature] = degrees Celcius
+  float pressure; // [pressure] = hPa
+  float humidity; // [humidity] = % RH
+  float altitudeGPS; // [altitude] = m
+  float altitudePressure; // [altitude] = m
+  float latitude; // [latitude] = degrees N
+  float longitude; // [longitude] = degrees E
+  int lightLevel;
+  float batteryVoltage; // [voltage] = V
+  float motorOutputVoltage; // [voltage] = V
+  vector3D gyro; // [gyro] = DPS
+  vector3D acceleration; // [acceleration] = g
+  float angularSpeed; // [angular speed] = RPM
+};
 
 // Pin definitions
 const int PIN_LORA_CS = 3;     // LoRa radio chip select
@@ -23,20 +48,12 @@ const char *password = "9349055587";
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws"); // WebSocket endpoint
 
-String receivedData = "No Data Yet"; // Store received LoRa data
+dataContainer receivedData;
 
-// WebSocket event handler
-void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-  if (type == WS_EVT_CONNECT) {
-    Serial.println("WebSocket client connected");
-    client->text(receivedData); // Send current data to new client
-  }
-}
-
-// Send LoRa data to all WebSocket clients
-void sendWebSocketMessage(String message) {
-  ws.textAll(message);
-}
+String dataToJSON(void);
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+void sendWebSocketMessage(String message);
+void parseLoRaData(String rawData);
  
 void setup() {
   Serial.begin(9600);
@@ -57,7 +74,7 @@ void setup() {
   }
  
   // Setup LoRa module
-  /**LoRa.setPins(PIN_LORA_CS, PIN_LORA_RESET, PIN_LORA_DIO0);
+  LoRa.setPins(PIN_LORA_CS, PIN_LORA_RESET, PIN_LORA_DIO0);
  
   if (!LoRa.begin(LORA_FREQUENCY)) {
     Serial.println("Starting LoRa failed!");
@@ -66,13 +83,13 @@ void setup() {
   }
 
   LoRa.setSignalBandwidth(LORA_SIGNAL_BANDWITH);
-  LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);**/
+  LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
 
   // WebSocket setup
   ws.onEvent(onWebSocketEvent);
   server.addHandler(&ws);
 
-  // Serve HTML, CSS, and JS from SPIFFS
+  // Serve HTML, CSS, and JS from Little FS
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(LittleFS, "/index.html", "text/html");
   });
@@ -82,33 +99,140 @@ void setup() {
   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(LittleFS, "/script.js", "application/javascript");
   });
-
   server.on("/cyberfreight.otf", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(LittleFS, "/cyberfreight.otf", "font/otf");
   });
 
   server.begin();
 }
+
  
 void loop() {
   // Try to parse packet
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    // Received a packet
-    Serial.print("Received '");
- 
-    // Read packet
+    String rawData = "";
     while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-      receivedData += (char)LoRa.read();
+      rawData += (char)LoRa.read();
     }
-
-    Serial.println("Received LoRa Data: " + receivedData);
-    sendWebSocketMessage(receivedData);
  
-    // Print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
+    parseLoRaData(rawData);
+    String jsonData = dataToJSON();
+    sendWebSocketMessage(jsonData);
   }
   ws.cleanupClients();
+}
+
+
+String dataToJSON(void) {
+  String jsonData = "{";
+  jsonData += "\"tickCount\":" + String(receivedData.tickCount) + ",";
+  jsonData += "\"temperature\":" + String(receivedData.temperature) + ",";
+  jsonData += "\"pressure\":" + String(receivedData.pressure) + ",";
+  jsonData += "\"humidity\":" + String(receivedData.humidity) + ",";
+  jsonData += "\"altitudeGPS\":" + String(receivedData.altitudeGPS) + ",";
+  jsonData += "\"altitudePressure\":" + String(receivedData.altitudePressure) + ",";
+  jsonData += "\"latitude\":" + String(receivedData.latitude) + ",";
+  jsonData += "\"longitude\":" + String(receivedData.longitude) + ",";
+  jsonData += "\"lightLevel\":" + String(receivedData.lightLevel) + ",";
+  jsonData += "\"batteryVoltage\":" + String(receivedData.batteryVoltage) + ",";
+  jsonData += "\"motorOutputVoltage\":" + String(receivedData.motorOutputVoltage) + ",";
+  jsonData += "\"gyro\":{\"x\":" + String(receivedData.gyro.x) + ",\"y\":" + String(receivedData.gyro.y) + ",\"z\":" + String(receivedData.gyro.z) + "},";
+  jsonData += "\"acceleration\":{\"x\":" + String(receivedData.acceleration.x) + ",\"y\":" + String(receivedData.acceleration.y) + ",\"z\":" + String(receivedData.acceleration.z) + "},";
+  jsonData += "\"angularSpeed\":" + String(receivedData.angularSpeed);
+  jsonData += "}";
+  return jsonData;
+}
+
+
+// WebSocket event handler
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.println("WebSocket client connected");
+      client->text("Connected to ESP32 WebSocket server!");
+      break;
+
+    case WS_EVT_DISCONNECT:
+      Serial.println("WebSocket client disconnected");
+      break;
+
+    case WS_EVT_DATA:
+      Serial.println("Received WebSocket data:");
+      Serial.write(data, len);
+      Serial.println();
+      break;
+          
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+
+// Send LoRa data to all WebSocket clients
+void sendWebSocketMessage(String message) {
+  ws.textAll(message);
+}
+
+
+void parseLoRaData(String rawData) {
+  char buf[rawData.length() + 1];
+  rawData.toCharArray(buf, sizeof(buf));
+
+  char *token = strtok(buf, ";");
+  if (token == NULL) return;
+
+  receivedData.tickCount = atol(token);
+
+  token = strtok(NULL, ";");
+  receivedData.temperature = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.pressure = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.humidity = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.altitudeGPS = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.altitudePressure = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.latitude = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.longitude = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.lightLevel = atoi(token);
+
+  token = strtok(NULL, ";");
+  receivedData.batteryVoltage = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.motorOutputVoltage = atof(token);
+
+  token = strtok(NULL, ";");
+  receivedData.gyro.x = atof(token);
+  
+  token = strtok(NULL, ";");
+  receivedData.gyro.y = atof(token);
+  
+  token = strtok(NULL, ";");
+  receivedData.gyro.z = atof(token);
+  
+  token = strtok(NULL, ";");
+  receivedData.acceleration.x = atof(token);
+  
+  token = strtok(NULL, ";");
+  receivedData.acceleration.y = atof(token);
+  
+  token = strtok(NULL, ";");
+  receivedData.acceleration.z = atof(token);
+  
+  token = strtok(NULL, ";");
+  receivedData.angularSpeed = atof(token);
 }
